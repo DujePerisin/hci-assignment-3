205f67624fc3e6fc4d1f9fca9e532a2050e592bd
DujePerisin <dperis00@fesb.hr>
2024-11-06 23:05:52 +0100

    Correcting my file-naming mistake and "stricting up" the naming convention for pages.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,47 @@
+//Hardcoding for testing..
+type Page = {
+  title: string;
+  path: `/${string}`;
+}
+
+const pages: Page[] = [
+  {title: "Home",
+    path: "/"
+  },
+  {title: "About me",
+    path: "/about",
+  },
+  {title: "Research Blogs",
+    path: "/blog",
+  },
+  {title: "Contact",
+    path: "/contact",
+  },
+  {title: "Education",
+    path: "/education",
+  },
+  {title: "Forum",
+    path: "/forum",
+  },
+  {title: "Business Travels",
+    path: "/travel",
+  }
+];
+
+
+
+function processPage(page: Page, index: number) {
+  return (
+    <li key={index}>
+      <a href={page.path}>{page.title}</a>
+    </li>
+  );
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
+            <ul className="flex space-x-4 mb-4">{pages.map(processPage)}</ul>
       <h1 className="text-6xl font-extrabold tracking-tight">Home page</h1>
     </main>
   )
