2e3e4b5e556ffc4f440ed8ea3ba98d7d3d6e86d4
DujePerisin <dperis00@fesb.hr>
2024-11-06 22:27:48 +0100

    Creating main pages.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-10">
+      <h1 className="text-6xl font-extrabold tracking-tight">About me</h1>
+    </main>
+  )
+}
