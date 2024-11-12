89929d88249cf7cf81917b1adbfa740b6834ea9d
DujePerisin <dperis00@fesb.hr>
2024-11-12 16:45:54 +0100

    Writing down future development plans on each tab.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,8 @@ export default function About() {
     <main className="flex min-h-screen flex-col items-center p-10">
       
       <h1 className="text-6xl font-extrabold tracking-tight">About me</h1>
+      <p className="text-2xl font-mono mt-6">Will all be on one single site.....</p>
+      <p className="text-2xl font-mono">....in future development</p>
     </main>
   )
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,6 +3,9 @@ export default function Blog() {
     <main className="flex min-h-screen flex-col items-center p-10">
       
       <h1 className="text-6xl font-extrabold tracking-tight">Research blogs</h1>
+      <p className="text-2xl font-mono mt-6">Considering implementing a "filter-based search" for all the blogs.....</p>
+      <p className="text-2xl font-mono">....in future development</p>
+
     </main>
   )
 }
